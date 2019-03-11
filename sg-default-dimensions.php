2fba145ae837f9c9d781cc319126c579552bf824
Jeff @ Pye Brook <jeff@pyebrook.com>
2019-03-11 15:20:32 -0400

    calculate weight from rhinestone count Determine product type using woo product api

--- sg-default-dimensions.php
+++ sg-default-dimensions.php
@@ -48,30 +48,54 @@ add_filter( 'woocommerce_product_variation_get_height', 'xa_product_default_heig
 if( ! function_exists('xa_product_default_height')) {
 	function xa_product_default_height( $height) {
 
-		$default_height = 1.99;			// Provide default Height
+		$default_height = .125;			// Provide default Height
 		if( empty($height) ) {
 			return $default_height;
-		}
-		else {
+		} else {
 			return $height;
 		}
 	}
 }
 
 // To set Default Weight
-add_filter( 'woocommerce_product_get_weight', 'xa_product_default_weight' );
-add_filter( 'woocommerce_product_variation_get_weight', 'xa_product_default_weight' );	// For variable product variations
+add_filter( 'woocommerce_product_get_weight', 'xa_product_default_weight', 10, 2  );
+add_filter( 'woocommerce_product_variation_get_weight', 'xa_product_default_weight', 10, 2 );	// For variable product variations
 
 if( ! function_exists('xa_product_default_weight') ) {
-	function xa_product_default_weight( $weight) {
+	/**
+	 * @param float $weight
+	 * @param WC_Product $wc_product_thing
+	 *
+	 * @return float
+	 */
+	function xa_product_default_weight( $weight, $wc_product_thing ) {
+		if ( sg_is_a_design( $wc_product_thing->get_id() ) ) {
+			$sg_design = new SG_Design_Product( $wc_product_thing->get_id() );
+			if ( $gem_count = $sg_design->get_bling_rhinestone_count() ) {
 
-		$default_weight = 0.75;			// Provide default Weight
-		if( empty($weight) ) {
-			return $default_weight;
+				// 0.03 lbs per 1000 rhinestones
+				$sg_rhinestone_weight_per_1000 = get_option( 'sg_rhinestone_weight_per_1000', 0.03 );
+
+				// 0.16 lbs per 16 inch x 16 inch transfer sheet
+				$sg_rhinestone_carrier_weight = get_option( 'sg_rhinestone_carrier_weight', 0.1 );
+
+				// weight in pounds
+				$weight = $sg_rhinestone_carrier_weight + $gem_count * $sg_rhinestone_weight_per_1000 / 1000;
+
+				// round this up to the nearest ounce
+				$ounces = $weight * 16;
+				$ounces = ceil( $ounces );
+				$weight = round( $ounces / 16, 1 );
+
+			}
 		}
-		else {
-			return $weight;
+
+		if ( empty( $weight ) ) {
+			$sg_default_weight = get_option( 'sg_default_weight', 0.5 );
+			$weight            = $sg_default_weight;
 		}
+
+		return $weight; // pounds
 	}
 }
 
